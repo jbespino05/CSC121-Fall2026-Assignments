90e7afe9727d52bc165aefa0f0da68183caa30b9
jbespino05 <joseea.005@gmail.com>
2026-09-11 03:45:42 -0400

    MIDPOINT
    Fix syntax error in print statement and add age calculation.

--- Chapter 2 - Programming Exercise.py
+++ Chapter 2 - Programming Exercise.py
@@ -10,3 +10,14 @@
 # 3: Print the top of the border using string multiplication
 # 4: Print the message
 # 5: Print the bottom border
+
+Name = "Jose Espinoza"
+Goal = "Become a structural engineer"
+Birth_year = 2005
+Current_year = 2026
+age = Current_year - Birth_year
+border = "=" * 25
+
+print(border)
+print("My name is", Name, "and I am", age, "years old."
+
