cb7cc102b2d096996b6f5c207c239b2a45f4fa36
jbespino05 <joseea.005@gmail.com>
2026-09-11 03:20:09 -0400

    STRUCTURE & PSEUDOCODE
    Outline the variables and logic for the programming exercise.

---
+++ Chapter 2 - Programming Exercise.py
@@ -0,0 +1,12 @@
+# Variables:
+# Name
+# Goal
+# Birth_year
+# Current_year
+
+# Logic:
+# 1: Assign values to all the variables
+# 2: Calculate age
+# 3: Print the top of the border using string multiplication
+# 4: Print the message
+# 5: Print the bottom border
