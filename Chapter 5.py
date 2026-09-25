4827046450aefb365f2c93019506683476c5629a
jbespino05 <joseea.005@gmail.com>
2026-09-25 13:17:34 -0400

    STRUCTURE & PSEUDOCODE

---
+++ Chapter 5.py
@@ -0,0 +1,14 @@
+# IMPORT STATEMENTS
+# (NONE)
+
+# FUNCTION DEFINITIONS 
+# 1. Define calculate_ticket(age, hour)
+# 2. Logic: If age <= 12 or age >= 65, price = 8. Otherwise, price = 12.
+# 3. Logic: If hour < 17, subtract 2 from the price.
+# 4. Print the final price.
+
+# PROGRAM LOGIC
+# 1. input() for age (int)
+# 2. input() for movie hour (int)
+# 3. Call calculate_ticket()
+
